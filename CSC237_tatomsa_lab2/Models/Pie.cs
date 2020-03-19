b15896faf94822ce80bd9b89104991198b08b673
Tesafye Atomsa <tatomsa@student.cccs.edu>
2020-03-18 23:53:13 -0600

    creating project and cloning the project

---
+++ CSC237_tatomsa_lab2/Models/Pie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSC237_tatomsa_lab2.Models
+{
+    public class Pie
+    {
+    }
+}
